ff0090a9ec1464fcbc5d2c8659640506735db69b
Pootiszz <39996037+Pootiszz@users.noreply.github.com>
2025-10-05 17:40:18 +0200

    Add files via upload

---
+++ PalindromeAlt.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class PalindromeAlt {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter line to check: ");
+        String original = scanner.nextLine();
+        String tekstUdenMellemrum = original.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String reversed = new StringBuilder(tekstUdenMellemrum).reverse().toString();
+        
+       
+        if (tekstUdenMellemrum.equals(reversed)) {
+            System.out.println("'" + original + "' is a palindrome!");
+        } else {
+            System.out.println("'" + original + "' is not a palindrome.");
+        }
+        
+        scanner.close();
+    }
+}
